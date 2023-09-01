d93f7b1e6f6cf031a72c7d8d7f33b6633c2136de
Kayla Reopelle <kreopelle@newrelic.com>
2023-09-01 16:24:58 -0700

    Add LogRecordExporter

--- logs_sdk/lib/opentelemetry/sdk/logs/export.rb
+++ logs_sdk/lib/opentelemetry/sdk/logs/export.rb
@@ -25,3 +25,4 @@ end
 require_relative 'export/log_record_processor'
 require_relative 'export/simple_log_record_processor'
 require_relative 'export/batch_log_record_processor'
+require_relative 'export/log_record_exporter'
