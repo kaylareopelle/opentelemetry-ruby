78456916d9cddce20591ef0d160928b6a680d0a1
Kayla Reopelle <kreopelle@newrelic.com>
2023-09-01 16:25:41 -0700

    Remove processor from logs.rb

--- logs_sdk/lib/opentelemetry/sdk/logs.rb
+++ logs_sdk/lib/opentelemetry/sdk/logs.rb
@@ -7,7 +7,6 @@
 require_relative 'logs/version'
 require_relative 'logs/logger'
 require_relative 'logs/logger_provider'
-require_relative 'logs/log_record_processor'
 require_relative 'logs/export'
 require_relative 'logs/log_record'
 require_relative 'logs/log_record_data'
