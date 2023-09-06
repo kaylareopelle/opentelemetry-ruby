5f7a0514838c9e0e95b13d071f3afb7e76ba096e
Kayla Reopelle <kreopelle@newrelic.com>
2023-09-06 14:37:13 -0700

    Add tests for simple log record processor

--- logs_sdk/test/opentelemetry/sdk/logs/export/simple_log_record_processor_test.rb
+++ logs_sdk/test/opentelemetry/sdk/logs/export/simple_log_record_processor_test.rb
@@ -7,53 +7,142 @@
 require 'test_helper'
 
 describe OpenTelemetry::SDK::Logs::Export::SimpleLogRecordProcessor do
+  let(:exporter) { OpenTelemetry::SDK::Logs::Export::LogRecordExporter.new }
+  let(:processor) { OpenTelemetry::SDK::Logs::Export::SimpleLogRecordProcessor.new(exporter) }
+  let(:log_record) { OpenTelemetry::SDK::Logs::LogRecord.new }
+  let(:mock_context) { Minitest::Mock.new }
+
   describe '#initialize' do
-    it 'raises an error if exporter is invalid' do
+    it 'raises an error when exporter is invalid' do
+      OpenTelemetry::Common::Utilities.stub(:valid_exporter?, false) do
+        assert_raises(ArgumentError) { OpenTelemetry::SDK::Logs::Export::SimpleLogRecordProcessor.new(exporter) }
+      end
     end
   end
 
   describe '#emit' do
-    it 'does not emit if stopped' do
+    let(:sampled_span_context) { OpenTelemetry::Trace::SpanContext.new(trace_flags: OpenTelemetry::Trace::TraceFlags::SAMPLED) }
+    let(:log_record) { OpenTelemetry::SDK::Logs::LogRecord.new(span_context: sampled_span_context) }
+
+    it 'exports the log records' do
+      mock_exporter = Minitest::Mock.new
+      processor.instance_variable_set(:@log_record_exporter, mock_exporter)
+      mock_log_record_data = Minitest::Mock.new
+
+      log_record.stub(:to_log_record_data, mock_log_record_data) do
+        OpenTelemetry::Common::Utilities.stub(:valid_exporter?, true) do
+          mock_exporter.expect(:export, OpenTelemetry::SDK::Logs::Export::SUCCESS, [[mock_log_record_data]])
+          processor.emit(log_record, mock_context)
+          mock_exporter.verify
+        end
+      end
     end
 
-    it 'does not emit unless sampled' do
+    it 'does not export if stopped' do
+      processor.instance_variable_set(:@stopped, true)
+      # raise if export is invoked
+      exporter.stub(:export, ->(_) { raise 'whoops!' }) do
+        processor.emit(log_record, mock_context)
+      end
     end
 
-    it 'converts the log records to LogRecordData when sampled' do
+    it 'does not export if log_record is nil' do
+      # raise if export is invoked
+      exporter.stub(:export, ->(_) { raise 'whoops!' }) do
+        processor.emit(nil, mock_context)
+      end
+    end
+
+    it 'does not raise if exporter is nil' do
+      processor.instance_variable_set(:@log_record_exporter, nil)
+      processor.emit(log_record, mock_context)
+    end
+
+    it 'does not export unless sampled' do
+      # SpanContext's default trace_flags are not sampled
+      log_record.instance_variable_set(:@span_context, OpenTelemetry::Trace::SpanContext.new)
+      refute(log_record.span_context.trace_flags.sampled?)
+      # raise if exporter's emit call is invoked
+      exporter.stub(:export, ->(_) { raise 'whoops!' }) do
+        processor.emit(log_record, mock_context)
+      end
     end
 
     it 'catches and logs exporter errors' do
-    end
+      error_message = 'uh oh'
+      log_record.span_context = sampled_span_context
+      logger_mock = Minitest::Mock.new
+      logger_mock.expect(:error, nil, [/#{error_message}/])
+      # raise if exporter's emit call is invoked
+      OpenTelemetry.stub(:logger, logger_mock) do
+        exporter.stub(:export, ->(_) { raise error_message }) do
+          processor.emit(log_record, mock_context)
+        end
+      end
 
-    it 'calls export on the log records' do
+      logger_mock.verify
     end
   end
 
   describe '#force_flush' do
     it 'does not attempt to flush if stopped' do
+      processor.instance_variable_set(:@stopped, true)
+      # raise if export is invoked
+      exporter.stub(:force_flush, ->(_) { raise 'whoops!' }) do
+        processor.force_flush
+      end
     end
 
     it 'returns success when the exporter cannot be found' do
+      processor.instance_variable_set(:@log_record_exporter, nil)
+      assert_equal(OpenTelemetry::SDK::Logs::Export::SUCCESS, processor.force_flush)
     end
 
     it 'calls #force_flush on the exporter' do
+      exporter = Minitest::Mock.new
+      processor.instance_variable_set(:@log_record_exporter, exporter)
+      exporter.expect(:force_flush, nil, timeout: nil)
+      processor.force_flush
+      exporter.verify
     end
   end
 
   describe '#shutdown' do
     it 'does not attempt to shutdown if stopped' do
+      processor.instance_variable_set(:@stopped, true)
+      # raise if export is invoked
+      exporter.stub(:shutdown, ->(_) { raise 'whoops!' }) do
+        processor.shutdown
+      end
     end
 
-    it 'returns success when the exporter cannot be found' do
-    end
+    describe 'when exporter is nil' do
+      it 'returns success' do
+        processor.instance_variable_set(:@log_record_exporter, nil)
+        assert_equal(OpenTelemetry::SDK::Logs::Export::SUCCESS, processor.shutdown)
+      end
 
-    it 'sets stopped to true when the exporter cannot be found' do
+      it 'sets stopped to true' do
+        processor.instance_variable_set(:@log_record_exporter, nil)
+        processor.shutdown
+        assert(processor.instance_variable_get(:@stopped))
+      end
     end
 
     it 'calls shutdown on the exporter' do
+      exporter = Minitest::Mock.new
+      processor.instance_variable_set(:@log_record_exporter, exporter)
+      exporter.expect(:shutdown, nil, timeout: nil)
+      processor.shutdown
+      exporter.verify
     end
 
     it 'sets stopped to true after calling shutdown on the exporter' do
+      exporter = Minitest::Mock.new
+      processor.instance_variable_set(:@log_record_exporter, exporter)
+      exporter.expect(:shutdown, nil, timeout: nil)
+      processor.shutdown
+      assert(processor.instance_variable_get(:@stopped))
     end
   end
 end
