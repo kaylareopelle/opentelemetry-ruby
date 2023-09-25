da0027859db8972f4bcdb51914466f639e2766b6
Kayla Reopelle <kreopelle@newrelic.com>
2023-09-25 11:16:23 -0700

    Add instrumentation_regirstry to LoggerProvider
    Also, refactor error messages to not be stored in Constants

--- logs_sdk/lib/opentelemetry/sdk/logs/logger_provider.rb
+++ logs_sdk/lib/opentelemetry/sdk/logs/logger_provider.rb
@@ -9,12 +9,14 @@ module OpenTelemetry
     module Logs
       # The SDK implementation of OpenTelemetry::Logs::LoggerProvider.
       class LoggerProvider < OpenTelemetry::Logs::LoggerProvider
-        attr_reader :resource, :log_record_processors, :log_record_limits
+        Key = Struct.new(:name, :version)
+        private_constant(:Key)
 
         UNEXPECTED_ERROR_MESSAGE = 'unexpected error in ' \
           'OpenTelemetry::SDK::Logs::LoggerProvider#%s'
 
         private_constant :UNEXPECTED_ERROR_MESSAGE
+        attr_reader :resource, :log_record_processors, :log_record_limits
 
         # Returns a new LoggerProvider instance.
         #
@@ -32,6 +34,8 @@ module OpenTelemetry
           @mutex = Mutex.new
           @resource = resource
           @stopped = false
+          @registry = {}
+          @registry_mutex = Mutex.new
         end
 
         # Returns an {OpenTelemetry::SDK::Logs::Logger} instance.
@@ -40,12 +44,20 @@ module OpenTelemetry
         # @param [optional String] version Instrumentation package version
         #
         # @return [OpenTelemetry::SDK::Logs::Logger]
-        def logger(name:, version: nil)
-          version ||= ''
+        def logger(name = nil, version = nil)
+          if @stopped
+            OpenTelemetry.logger.warn('calling LoggerProvider#logger after shutdown, a noop logger will be returned')
+            OpenTelemetry::Logs::LoggerProvider::NOOP_LOGGER
+          else
+            version ||= ''
 
-          if !name.is_a?(String) || name.empty?
+            if !name.is_a?(String) || name.empty?
             OpenTelemetry.logger.warn('LoggerProvider#logger called with an ' \
               "invalid name. Name provided: #{name.inspect}")
+
+            @registry_mutex.synchronize do
+              @registry[Key.new(name, version)] ||= Logger.new(name, version, self)
+            end
           end
 
          Logger.new(name, version, self)
