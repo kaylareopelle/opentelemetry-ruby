fc7d7f5214ce93ef7c6adc2090ba8b73f2c16101
Kayla Reopelle <kreopelle@newrelic.com>
2023-10-03 15:15:19 -0700

    Document log record limits arg on LoggerProvider#initialize

--- logs_sdk/lib/opentelemetry/sdk/logs/logger_provider.rb
+++ logs_sdk/lib/opentelemetry/sdk/logs/logger_provider.rb
@@ -22,6 +22,10 @@ module OpenTelemetry
         #
         # @param [optional Resource] resource The resource to associate with
         #   new LogRecords created by {Logger}s created by this LoggerProvider.
+        # @param [optional Array] log_record_processors The
+        #   {LogRecordProcessor}s to associate with this LoggerProvider.
+        # @param [optional LogRecordLimits] log_record_limits The limits for
+        #   attributes count and attribute length for LogRecords.
         #
         # @return [OpenTelemetry::SDK::Logs::LoggerProvider]
         def initialize(
