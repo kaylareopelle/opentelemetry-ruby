cd1981e175783360db47f7cf3ada9817338e011e
Kayla Reopelle <kreopelle@newrelic.com>
2024-10-16 10:51:54 -0700

    test: add bottom stars

--- exporter/otlp-metrics/test/opentelemetry/exporter/otlp/metrics/metrics_exporter_test.rb
+++ exporter/otlp-metrics/test/opentelemetry/exporter/otlp/metrics/metrics_exporter_test.rb
@@ -644,6 +644,7 @@ describe OpenTelemetry::Exporter::OTLP::Metrics::MetricsExporter do
       assert_requested(:post, 'http://localhost:4318/v1/metrics') do |req|
         req.body == Zlib.gzip(encoded_etsr) # is asserting that the body of the HTTP request is equal to the result of gzipping the encoded_etsr.
       end
+      puts '*********************************************'
     end
   end
 end
